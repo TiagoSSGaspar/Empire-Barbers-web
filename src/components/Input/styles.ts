51a6688e4928670f99c031ed1cde6178cc54d42a
Tiago S S Gaspar <tiago.sousasilvagaspar@gmail.com>
2021-03-22 18:20:45 -0300

    Criando tooltip de erros

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,8 +1,11 @@
 import styled, { css } from 'styled-components';
 
+import Tooltip from '../Tooltip';
+
 interface ContainerProps {
   isFocused: boolean;
   isFilled: boolean;
+  isErrored: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -18,6 +21,9 @@ export const Container = styled.div<ContainerProps>`
   & + div {
     margin-top: 8px;
   }
+  ${props => props.isErrored && css`
+    border-color: #c53030;
+  `}
 
   ${props => props.isFocused && css`
     color: #ff9000;
@@ -27,6 +33,7 @@ export const Container = styled.div<ContainerProps>`
   ${props => props.isFilled && css`
     color: #ff9000;
   `}
+
 
   input {
     background: transparent;
@@ -42,5 +49,23 @@ export const Container = styled.div<ContainerProps>`
   }
   svg {
     margin-right: 16px;
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #fff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
   }
 `;
